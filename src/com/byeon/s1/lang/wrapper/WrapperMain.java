ddc0736c7d636a6e9f65f0f02977b9f1f2dd6d17
BJSSSSSS <john0222223@gmail.com>
2022-01-11 15:03:15 +0900

    String(2022.01.11)3

---
+++ src/com/byeon/s1/lang/wrapper/WrapperMain.java
@@ -0,0 +1,26 @@
+
+
+package com.byeon.s1.lang.wrapper;
+
+public class WrapperMain {
+
+	public static void main(String[] args) {
+		System.out.println(Integer.BYTES); //1byte(8bit)
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		System.out.println(Integer.SIZE);  // bit
+		
+		
+		System.out.println(Long.BYTES);
+
+	}
+
+}
+
+
+
+
+
+
+
+
