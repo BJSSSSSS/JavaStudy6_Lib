b75a25229e558e7912adb8264776e69ebf4881b4
BJSSSSSS <john0222223@gmail.com>
2022-01-13 16:08:17 +0900

    map(2022.01.13)2

--- src/com/byeon/s3/ex1/Ex1Main.java
+++ src/com/byeon/s3/ex1/Ex1Main.java
@@ -3,8 +3,10 @@
 package com.byeon.s3.ex1;
 
 
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 import com.byeon.s3.ex1.member.MemberDTO;
 import com.byeon.s3.ex1.member.MemberData;
@@ -24,25 +26,35 @@ public class Ex1Main {
 		//지금 여기에 나온 값은 hs야
 		//map으로 받아준다 왜냐 부모클래스로 받으면 다형성이 좋기 때문
 		//나는 hashmap으로 받아버려서 강제 형변환을 해줬어야 했다!
-		System.out.println(map);
-		
-		
-		for(int i=0; i < map.size(); i++) {
-			StudentDTO stu = new StudentDTO();
-			stu = map.get(ar.get(i).getId());
-			System.out.println("ID : " + stu.getId());
-			System.out.println("NAME : " + stu.getName());
-			System.out.println("KOR : " + stu.getKor());
-			System.out.println("ENG : " + stu.getEng());
-			System.out.println("MATH : " + stu.getMath());
-			System.out.println("TOTAL : " + stu.getTotal());
-			System.out.println("AVG : " + stu.getAvg());
-			System.out.println();
+		//System.out.println(map);
+		Set<String> keys = map.keySet();
+		Iterator<String> k = keys.iterator();
+
+		while(k.hasNext()) {
+			String key = k.next();
+			StudentDTO studentDTO = map.get(key);
+			System.out.println("Key : " + key);
+			System.out.println(studentDTO.getId());
+			System.out.println(studentDTO.getName());
+			System.out.println(studentDTO.getKor());
+			System.out.println(studentDTO.getEng());
+			System.out.println(studentDTO.getMath());
+			System.out.println(studentDTO.getTotal());
+			System.out.println(studentDTO.getAvg());
+			System.out.println("====================");
 		}
-		
 		
 	}
 
 }
+
+
+
+
+
+
+
+
+
 
 
